1b4412d4c78d8b23518ddcaabbaf2ce921073fd2
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2022-10-04 11:07:55 +0200

    Initial commit

---
+++ src/dk/javahandson/gui/MainController.java
@@ -0,0 +1,14 @@
+package dk.javahandson.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label lbl;
+
+    public void clickBtn(ActionEvent actionEvent) {
+        lbl.setText("Huh?");
+    }
+}
